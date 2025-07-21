8e88c79b37fa04637a27cf7e6d2d7be4c6d25c3a
michael-bailey <bailey-michael1@outlook.com>
2025-07-21 10:08:18 +0100

    Created basic privacy policy filter classes

---
+++ backend/src/main/kotlin/io/github/michael_bailey/spring_blog/privacy/policy/RequestAnalyticsPolicy.kt
@@ -0,0 +1,17 @@
+package io.github.michael_bailey.spring_blog.privacy.policy
+
+import io.github.michael_bailey.spring_blog.model.RequestAnalyticsModel
+import io.github.michael_bailey.spring_blog.privacy.rules.AllowIfRequestCreatedRule
+import io.github.michael_bailey.spring_blog.privacy.rules.DenyAllRule
+import io.github.michael_bailey.spring_blog.privacy.rules.PrivacyRule
+import io.github.michael_bailey.spring_blog.security.viewer.IViewerContext
+
+class RequestAnalyticsPolicy(vc: IViewerContext,
+): PrivacyPolicy<RequestAnalyticsModel>(vc = vc) {
+
+	override val queryPolicy: List<PrivacyRule<RequestAnalyticsModel>> = listOf(
+		AllowIfRequestCreatedRule(),
+		DenyAllRule()
+	)
+
+}
