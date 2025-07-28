3f9b6d3dfc938506a2d44f81707c3275bbdb1766
michael-bailey <bailey-michael1@outlook.com>
2025-07-28 13:22:28 +0100

    Setup basic cookie toggles in settings page

--- frontend/src/app/settings/PrivacyCookieToggles.tsx
+++ frontend/src/app/settings/PrivacyCookieToggles.tsx
@@ -1,17 +1,53 @@
+'use client'
 
+import {ReactElement, useCallback, useEffect, useMemo, useState} from "react";
 
-import {ReactElement} from "react";
-import {getApiBase} from "@/lib/environment";
+export default function PrivacyCookieToggles(): ReactElement {
 
-export default async function PrivacyCookieToggles(): Promise<ReactElement> {
+	const [analytics, setAnalytics] = useState({
+		domain: null,
+		request: null,
+	})
 
-	const privacyPreferences = await fetch(`${getApiBase()}/api/analytics`).then(res => res.json())
+	const fetchCookies = useCallback(async () => {
+		const res = await fetch(`/api/analytics`)
+			.then(res => res.json())
+
+		setAnalytics({
+			domain: res["domain"],
+			request: res["request"]
+		})
+
+	}, [setAnalytics])
+
+	const domainEnabled = useMemo(() => analytics.domain !== "null", [analytics.domain])
+	const requestEnabled = useMemo(() => analytics.request !== "null", [analytics.request])
+
+	const toggleDomain = useCallback(async () => {
+		const url = `/api/analytics/preferences?allowDomainLogging=${!domainEnabled}`
+		const res = await fetch(url, {method: "POST"})
+		const data = await res.json()
+		if (data == true) await fetchCookies()
+	}, [domainEnabled, fetchCookies])
+
+	const toggleRequest = useCallback(async () => {
+		const url = `/api/analytics/preferences?allowRequestLogging=${!requestEnabled}`
+		const res = await fetch(url, {method: "POST"})
+		const data = await res.json()
+		if (data == true) await fetchCookies()
+	}, [fetchCookies, requestEnabled])
+
+	useEffect(() => {
+		fetchCookies()
+	}, [fetchCookies])
 
 	return <>
+		<h2 className="text-xl">Domain Logging</h2>
+		<p>example: {analytics.domain}</p>
+		<button onClick={toggleDomain}>Toggle Domain</button>
+		<br />
 		<h2 className="text-xl">Request Logging</h2>
-		<p>example: {privacyPreferences["request"]}</p>
-
-		<h2 className="text-xl">Request Logging</h2>
-		<p>example: {privacyPreferences["request"]}</p>
+		<p>example: {analytics.request}</p>
+		<button onClick={toggleRequest}>Toggle Request</button>
 	</>;
 }
