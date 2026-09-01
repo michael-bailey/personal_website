07069229e8804c675e3c624d29f253555a285fa1
michael-bailey <bailey-michael1@outlook.com>
2026-09-01 17:35:01 +0100

    update(server): Added more content to scouting hobby section

--- modules/server/src/main/kotlin/home/repository/HobbyContentRepository.kt
+++ modules/server/src/main/kotlin/home/repository/HobbyContentRepository.kt
@@ -11,7 +11,8 @@ class HobbyContentRepository {
 	fun getContentSections(): List<ContentSection> = listOf(
 		ContentSection(
 			header = "Hobbies",
-			description = "This is work in progress, expect stuff to do with swimming, electronics, gym, and robotics\"\"\".trimIndent()",
+			description = """"This is work in progress, expect stuff to do with swimming, 
+				electronics, gym, and robotics""".trimIndent(),
 			articles = listOf(
 				getScoutingArticle(),
 				getCampingArticle(),
@@ -24,11 +25,27 @@ class HobbyContentRepository {
 		header = "Scouting", content = listOf(
 			ArticleContent.Paragraph(
 				"""
-				I've been a part of my local scout group, since beavers.
-			""".trimIndent()
+				I've been a part of the scouts since being a beaver. During This i have 
+				had loads of different experiences through our weekly meetings, camps, and
+				meeting others.
+				""".trimIndent()
+			),
+			ArticleContent.Paragraph(
+				"""
+				A lot of the skills revolve around team work, as well as more practical
+				skills such as fire lighting, shelter making, pioneering, archery, and rifling.
+				""".trimIndent()
+			),
+			ArticleContent.Paragraph(
+				"""
+				I am now a leader of my group, planning sessions, and being tech support.
+				Along with this i am the chairman of the group, ensuring everything is run 
+				smoothly behind the scenes.
+				""".trimIndent()
 			)
 		)
 	)
+
 
 
 	private fun getNavigationArticle(): Article = MultiMediaArticle(
