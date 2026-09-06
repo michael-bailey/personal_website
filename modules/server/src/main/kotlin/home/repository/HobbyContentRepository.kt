25cdcbadc38e35a9f14277ba51ac3087d71a69cc
michael-bailey <bailey-michael1@outlook.com>
2026-09-06 19:37:54 +0100

    update(server): Filed out the 3d printing section

--- modules/server/src/main/kotlin/home/repository/HobbyContentRepository.kt
+++ modules/server/src/main/kotlin/home/repository/HobbyContentRepository.kt
@@ -14,9 +14,9 @@ class HobbyContentRepository {
 			description = """"This is work in progress, expect stuff to do with swimming, 
 				electronics, gym, and robotics""".trimIndent(),
 			articles = listOf(
+				get3DPrintingArticle(),
 				getScoutingArticle(),
-				getCampingArticle(),
-				getNavigationArticle(),
+				getSportArticle(),
 			)
 		),
 	)
@@ -28,43 +28,68 @@ class HobbyContentRepository {
 				I've been a part of the scouts since being a beaver. During This i have 
 				had loads of different experiences through our weekly meetings, camps, and
 				meeting others.
-				""".trimIndent()
+			""".trimIndent()
 			),
 			ArticleContent.Paragraph(
 				"""
 				A lot of the skills revolve around team work, as well as more practical
 				skills such as fire lighting, shelter making, pioneering, archery, and rifling.
-				""".trimIndent()
+			""".trimIndent()
 			),
 			ArticleContent.Paragraph(
 				"""
 				I am now a leader of my group, planning sessions, and being tech support.
 				Along with this i am the chairman of the group, ensuring everything is run 
 				smoothly behind the scenes.
-				""".trimIndent()
-			)
-		)
-	)
-
-
-
-	private fun getNavigationArticle(): Article = MultiMediaArticle(
-		header = "Navigation", content = listOf(
-			ArticleContent.Paragraph(
-				"""
-				As part of scouting, I've learnt basic map reading and compass skills.
-				From this i gained an interest in more complex navigational skills and tools.
 			""".trimIndent()
-			)
+			),
 		)
 	)
 
-	private fun getCampingArticle(): Article = MultiMediaArticle(
-		header = "Camping", content = listOf(
+	private fun getSportArticle(): Article = MultiMediaArticle(
+		header = "Sports", content = listOf(
 			ArticleContent.Paragraph(
 				"""
-				Camping has been a bit of a mix for me. Whilst i enjoy it, I have hay fever
-				which can be a limit to how much i can do and focus on.
+				I'm an avid swimmer, boulderer, cyclist, and gym goer. I find each of these serve
+				a different purpose. Swimming is a mix of speed and stamina training, gym 
+				I do both cardio and weight training. And bouldering is body weight exercises,
+				and dexterity.
+			""".trimIndent()
+			),
+		)
+	)
+
+	private fun get3DPrintingArticle(): Article = MultiMediaArticle(
+		header = "3D Printing", content = listOf(
+			ArticleContent.Paragraph(
+				"""
+					I've been using 3d printing to learn how to use cad software, taking measurements,
+					and properly setup a 3d printer to make clean prints.
+				""".trimIndent()
+			), ArticleContent.Paragraph(
+				"""
+					I've designed some of my own prints to help out with family and friends.
+					these have been designed and printed to fix broken parts for tools. For
+					instance, a new clip for some secateurs, new ends for tent poles, and  
+					stops for assistance wheels for my dog. I have a couple projects i'm looking 
+					into designing.
+				""".trimIndent()
+			), ArticleContent.Paragraph(
+				"""
+				 First is a 'small' pipe organ. I have created a tuned percussion instrument 
+				 before and have understanding how wind instruments work. Using this i should 
+				 be able to design the pipes and internal tubing to make a small working 
+				 pipe organ. If this were to succeed, then mi could make multiple units 
+				 with different voices, to create a slightly larger one, maybe even with 
+				 stops.
+			""".trimIndent()
+			), ArticleContent.Paragraph(
+				"""
+				Second project is an enclosure for my Bramble Pi project. The raspberry pis
+				i'm currently using, standing on edge, with no support. Plus they do have
+				a screen connected to the master control plane pi. So having an enclosure
+				that can contain all of them in one place, alongside the screen would be
+				nice and more reassuring.
 			""".trimIndent()
 			)
 		)
